c94a7fb30fcc5e03221982dfc8228f3f220190fc
jmbul <jerald.mark.buljatin@gmail.com>
2026-02-14 11:35:46 +0800

    Created buggy / bugs controller

---
+++ API/Controllers/BugsController.cs
@@ -0,0 +1,37 @@
+﻿using Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class BugsController : BaseApiController
+{
+    [HttpGet("unauthorized")]
+    public IActionResult GetUnauthorized()
+    {
+        return Unauthorized();
+    }
+    
+    [HttpGet("badrequest")]
+    public IActionResult GetBadRequest()
+    {
+        return BadRequest("Not a good request");
+    }
+    
+    [HttpGet("notfound")]
+    public IActionResult GetNotFound()
+    {
+        return NotFound("Not found");
+    }
+    
+    [HttpGet("internalerror")]
+    public IActionResult GetInternalError()
+    {
+        throw new Exception("Internal error");
+    }
+    
+    [HttpPost("validationerror")]
+    public IActionResult GetValidationError(Product product)
+    {
+        return Ok();
+    }
+}
