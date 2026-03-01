5675968c9b05f38782508f73f8154abacf277675
jmbul <jerald.mark.buljatin@gmail.com>
2026-03-01 15:18:59 +0800

    Should be data

--- API/RequestHelpers/Pagination.cs
+++ API/RequestHelpers/Pagination.cs
@@ -5,6 +5,6 @@ public class Pagination<T>(int pageIndex, int pageSize, int count, IReadOnlyList
     public int PageInex { get; set; } = pageIndex;
     public int PageSize { get; set; } = pageSize;
     public int Count { get; set; } = count;
-    public IReadOnlyList<T> Date { get; set; } = data;
+    public IReadOnlyList<T> Data { get; set; } = data;
     
 }
